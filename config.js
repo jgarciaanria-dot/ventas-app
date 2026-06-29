534a105b3828b270a9e6da63f46c00d7d699a94a
jgarciaanria-dot <jgarcia.anria@gmail.com>
2026-06-29 14:45:56 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   contabilidad: {
-    scriptUrl: 'https://script.google.com/macros/s/AKfycbxojmJGjWKsbF4dHPJFiN4Sf40kPbkT2CMyVhU6SbMA1vz77d0Aoa1wkwab-4sXGtEn/exec'
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbxxTfeb-Kn_LIEhtTPKvlz0AUAiqtBtTIASayjjdS3JwVCRv6dm8DDIgdf6Iw1BvqtA/exec'
   }
 };
